574faa4dcb2a6812a852ee6095e0266cd068cfd6
JustToBeBetter <likeme@li.local>
2016-10-25 15:42:42 +0800

    Initial Commit

---
+++ BluetoothDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BluetoothDemo
+//
+//  Created by DBOX on 2016/10/25.
+//  Copyright © 2016年 DBOX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BluetoothDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BluetoothDemo
+//
+//  Created by DBOX on 2016/10/25.
+//  Copyright © 2016年 DBOX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
